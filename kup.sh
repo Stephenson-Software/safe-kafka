e33c0786775db40eec9d3b69e49abd6c0b727ab6
BuildTools <unconfigured@null.spigotmc.org>
2023-12-01 12:58:30 -0700

    Added kafka container health check

--- kup.sh
+++ kup.sh
@@ -16,5 +16,23 @@ fi
 # spin up kafka
 echo "Spinning up kafka..."
 docker-compose down --remove-orphans
-docker-compose up --remove-orphans --build
-docker-compose scale kafka=3
+docker-compose up --remove-orphans --build -d
+
+# show running containers
+docker ps
+
+# TODO: scale kafka up to 2-3 instances
+
+# TODO: verify that zookeeper container is running and can be reached
+
+# TODO: verify that kafka container is running and can be reached
+sleep 2
+kafkaLogCount=$(docker logs safe-kafka_kafka_1 | wc -l)
+echo "Number of kafka logs: $kafkaLogCount"
+if (( $kafkaLogCount < 10 )); then
+	echo "Kafka container is probably not running."
+fi
+
+if (( $kafkaLogCount > 9 )); then
+	echo "Kafka container is probably up!"
+fi
