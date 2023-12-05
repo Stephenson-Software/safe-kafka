200dd62473a23966b8bf4a41b1aa1ac51dfd0372
dmccoystephenson <dmccoystephenson@gmail.com>
2023-12-04 23:06:27 -0700

    Added license headers

--- kdown.sh
+++ kdown.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# **********************************************************************************
+# *   Copyright 2023 Daniel McCoy Stephenson
+# *
+# *   Licensed under the Apache License, Version 2.0 (the "License");
+# *   you may not use this file except in compliance with the License.
+# *   You may obtain a copy of the License at
+# *
+# *       http://www.apache.org/licenses/LICENSE-2.0
+# *
+# *   Unless required by applicable law or agreed to in writing, software
+# *   distributed under the License is distributed on an "AS IS" BASIS,
+# *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# *   See the License for the specific language governing permissions and
+# *   limitations under the License.
+# *********************************************************************************
+
 # colors
 b='\e[0;30m'
 w='\e[0;37m'
@@ -14,13 +30,7 @@ if [[ $DOCKER_HOST_IP == "" ]]; then
 	echo -e "${y}DOCKER_HOST_IP not set. Setting to $DOCKER_HOST_IP${nc}"
 fi
 
-# if kafka is not running, exit
-if [[ $(docker ps -f name=kafka -q) == "" ]]; then
-    echo -e "${g}Kafka is not running.${nc}"
-    exit 1
-fi
-
-echo -e "${y}Stopping kafka..."
+echo -e "${y}Stopping kafka and removing containers...${nc}"
 docker-compose down --remove-orphans
 
 # if kafka is not running, success
--- kup.sh
+++ kup.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# **********************************************************************************
+# *   Copyright 2023 Daniel McCoy Stephenson
+# *
+# *   Licensed under the Apache License, Version 2.0 (the "License");
+# *   you may not use this file except in compliance with the License.
+# *   You may obtain a copy of the License at
+# *
+# *       http://www.apache.org/licenses/LICENSE-2.0
+# *
+# *   Unless required by applicable law or agreed to in writing, software
+# *   distributed under the License is distributed on an "AS IS" BASIS,
+# *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# *   See the License for the specific language governing permissions and
+# *   limitations under the License.
+# *********************************************************************************
+
 # colors
 b='\e[0;30m'
 w='\e[0;37m'
@@ -14,7 +30,7 @@ ZOOKEEPER_THRESHOLD=10
 EXPECTED_KAFKA_CONTAINER_NAME="safe-kafka_kafka_1"
 KAFKA_THRESHOLD=100
 
-TIME_TO_WAIT_FOR_KAFKA_TO_START=10
+TIME_TO_WAIT_FOR_KAFKA_TO_START=30
 NETWORK_CARD_INTERFACE="eth0"
 
 TEST_TOPIC_NAME="test"
